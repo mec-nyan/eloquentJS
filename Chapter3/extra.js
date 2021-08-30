a10cc57bbe90758628ee9f662f95055d3e6a793e
mec-nyan <marianoraspberrypi@gmail.com>
2021-08-29 21:23:00 -0300

    added tests

--- Chapter3/extra.js
+++ Chapter3/extra.js
@@ -8,3 +8,12 @@ const sayHi = function(name){ return "Hi, " + name + "!" };
 
 // arrow function
 const double = x => x * 2;
+
+
+// Tests
+for (let i = 0; i < 10; ++i) console.log(`The square of ${i} is ${square(i)}`);
+
+let names = [ 'Ada', 'Grace', 'Bjarne' ];
+for (n of names) console.log(sayHi(n));
+
+for (let i = 0; i < 10; ++i) console.log(`The double of ${i} is ${double(i)}`);
